90781db2a5820ca9dd1adb4eb1459891d10f7168
orcunakkaya <berkayakkaya49@gmail.com>
2026-02-16 18:14:05 +0300

    storage useSaveActions added

--- services/storage.ts
+++ services/storage.ts
@@ -1,12 +1,47 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
-const SAVED_KEY = 'SAVED_ARTICLES';
+import { useQueryClient } from '@tanstack/react-query';
+import { Article } from '@/types/news';
+const SAVED_QUERY_KEY = 'SAVED_ARTICLES';
 
 export const getSavedArticles = async () => {
-  const data = await AsyncStorage.getItem(SAVED_KEY);
+  const data = await AsyncStorage.getItem(SAVED_QUERY_KEY);
   return data ? JSON.parse(data) : [];
 };
 
-export const saveArticles = async (articles: any[]) => {
-  await AsyncStorage.setItem(SAVED_KEY, JSON.stringify(articles));
+export const saveArticles = async (articles: Article[]) => {
+  await AsyncStorage.setItem(SAVED_QUERY_KEY, JSON.stringify(articles));
 };
+
+
+export function useSaveActions() {
+  const queryClient = useQueryClient();
+
+  const toggleSave = async (article: Article) => {
+    const current: Article[] = queryClient.getQueryData([SAVED_QUERY_KEY]) || [];
+
+    const exists = current.find(a => a.url === article.url);
+
+    let updated;
+
+    if (exists) {
+      updated = current.filter(a => a.url !== article.url);
+    } else {
+      updated = [article, ...current];
+    }
+
+    await AsyncStorage.setItem(
+      SAVED_QUERY_KEY,
+      JSON.stringify(updated)
+    );
+
+    queryClient.setQueryData([SAVED_QUERY_KEY], updated);
+  };
+
+  const isSaved = (url: string) => {
+    const current = queryClient.getQueryData<Article[]>([SAVED_QUERY_KEY]) || [];
+
+    return current.some(a => a.url === url);
+  };
+
+  return { toggleSave, isSaved };
+}
